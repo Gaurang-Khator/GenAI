fa490d457641b80dff513e5b629b0e5c3b5f7541
Gaurang Khator <gaurangkhator04@gmail.com>
2026-05-08 18:18:00 +0530

    char text splitter

--- RAG project/document loaders/test.py
+++ RAG project/document loaders/test.py
@@ -1,8 +1,25 @@
 from langchain_community.document_loaders import TextLoader
 
+from langchain_text_splitters import CharacterTextSplitter 
+
+splitter = CharacterTextSplitter(
+    separator = "",
+    chunk_size = 30,
+    chunk_overlap = 1
+)
+
 data = TextLoader(r"D:\Development\GenAI\RAG project\document loaders\notes.txt") #Creating object of TextLoader class 
 # print(data)
 
 docs = data.load()
 
-print(docs[0].page_content)
+chunks = splitter.split_documents(docs)
+
+# print(docs[0].page_content)
+
+# print(len(chunks))
+
+for i in chunks:
+    print(i.page_content)
+    print()
+    print()
