86a1c6094d1302064bda028a30a18c75e7f38cfc
Gaurang Khator <gaurangkhator04@gmail.com>
2026-05-21 18:20:22 +0530

    recurive character text splitter

--- RAG project/document loaders/pdf.py
+++ RAG project/document loaders/pdf.py
@@ -1,6 +1,14 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
 
 data = PyPDFLoader(r"D:\Development\GenAI\RAG project\document loaders\cs-fundamentals.pdf") #Creating object of PyPDFLoader class
 docs = data.load();
 
-print(len(docs))
+chunks = splitter.split_documents(docs)
+
+print(len(chunks))
--- RAG project/main.py
+++ RAG project/main.py
@@ -2,13 +2,14 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI 
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate 
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
 
 load_dotenv()
 
 model = ChatMistralAI(model="mistral-small-2506")
 
 # data = TextLoader(r"D:\Development\GenAI\RAG project\document loaders\notes.txt")                #Creating object of TextLoader class
-data = PyPDFLoader(r"D:\Development\GenAI\RAG project\document loaders\cs-fundamentals.pdf")       #Creating object of PyPDFLoader class
+data = PyPDFLoader(r"D:\Development\GenAI\RAG project\document loaders\deeplearning.pdf")       #Creating object of PyPDFLoader class
 docs = data.load()
 
 template = ChatPromptTemplate.from_messages([
