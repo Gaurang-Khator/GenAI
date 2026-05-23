5b5959f2870054915347b589d3897f53cb789eed
Gaurang Khator <gaurangkhator04@gmail.com>
2026-05-23 18:48:49 +0530

    retrievers - similarity and mmr search

---
+++ RAG project/retrievers/mmr.py
@@ -0,0 +1,44 @@
+#Similarity search and MMR (Maximal Marginal Relevance) search 
+
+from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from langchain_mistralai import MistralAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+docs = [
+    Document(page_content="Gradient Descent is an optimization algorithm used in machine learning"),
+    Document(page_content="Gradient Descent minimizes the loss function."),
+    Document(page_content="Gradient Descent is an optimiztion technique that minimizes the loss function"),
+    Document(page_content="Neural networks uses gradient descent for training"),
+    Document(page_content="Support vector machines are supervised learning algorithms.")
+]
+
+embedding_model = MistralAIEmbeddings(model="mistral-embed")
+
+vector_store = Chroma.from_documents(docs, embedding_model)
+
+similarity_search = vector_store.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
+
+print("\n====SIMILARITY SEARCH RESULTS====")
+
+similarity_docs = similarity_search.invoke("What is gradient descent?")
+
+for doc in similarity_docs:
+    print(doc.page_content)
+
+mmr_search = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 3}
+)
+
+print("\n====MMR SEARCH RESULTS====")
+
+mmr_docs = mmr_search.invoke("What is gradient descent?")
+
+for doc in mmr_docs:
+    print(doc.page_content)
