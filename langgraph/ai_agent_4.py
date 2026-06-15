ed70de1b0b89f816fa217efffc879da3cd14aed6
Gaurang Khator <gaurangkhator04@gmail.com>
2026-06-15 11:41:21 +0530

    mini project started

---
+++ langgraph/ai_agent_4.py
@@ -0,0 +1,79 @@
+# MINI PROJECT - DRAFTER
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langgraph.graph import START, END, StateGraph
+from langgraph.prebuilt import ToolNode
+from langgraph.graph.message import add_messages
+from langchain_mistralai import ChatMistralAI
+from langchain.tools import tool
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage, BaseMessage
+from typing import TypedDict, Annotated, Sequence
+
+
+document_content = ""
+
+class AgentState(TypedDict):
+    messages : Annotated[Sequence[BaseMessage], add_messages]
+
+@tool
+def update_tool(content: str) -> str:
+    """Updates the content with the provided content."""
+
+    global document_content
+    document_content = content
+    return f"Document updated successfully! Updated(current) content is: \n{document_content}"
+
+@tool
+def save_tool(filename: str) -> str:
+    """Save the current document to a text file and finish the process.
+    
+    Args: filename : The name of the text file.
+    """
+
+    global document_content
+
+    if not filename.endswith(".txt"):
+        filename = f"{filename}.txt"
+
+    try:
+        with open(filename, 'w') as file:
+            file.write(document_content)
+        
+        print("\nDocument saved successfully!")
+        return f"The document has been saved successfully to '{filename}'."
+    except Exception as e:
+        return f"Error saving file: \n{str(e)}"
+    
+
+tools = [update_tool, save_tool]
+
+llm = ChatMistralAI(model = "mistral-small-2506").bind_tools(tools)
+
+def our_agent(state: AgentState) -> AgentState:
+    
+    system_prompt = SystemMessage(content=f"""
+    You are a Drafter, a helpful writing assistant. You are going to help user update and modify documents.
+    
+    - If the user wants to update or modify content, use the 'update_tool' tool with the complete updated content.
+    - If the user wants to save and finish, use the 'save_tool' tool.
+    - Make sure to always show the current document state after modifications.
+                                  
+    The current document content is: {document_content}
+    """)
+
+    if not state['messages']:
+        user_input = input("I'm ready to help you update a document. What would you like to create?")
+        user_message = HumanMessage(content=user_input)
+
+    else:
+        user_input = input("\nWhat would you like to do with the document? ")
+        print("\n User: ", user_input)
+        user_message = HumanMessage(content=user_input)
+
+    all_messages = [system_prompt] + list(state['messages']) + [user_message]
+    
+    response = llm.invoke(all_messages)
+
+    
